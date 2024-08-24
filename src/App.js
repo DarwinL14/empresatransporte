33e85e4e364b8cce6144ba1346f44bbd4ab8d955
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-08-23 19:51:35 -0500

    Se agrego archivo api y se hicieron ajustes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Index from "./pages/auth/Index";
 import Camiones from "./pages/auth/Camiones";
 import Login from "./pages/auth/Login";
+import Registro from "./pages/auth/Registro";
 import Carga from "./pages/auth/Carga";
 import Empresas from "./pages/auth/Empresas";
 import Cargocol from "./pages/auth/empresa/Cargocol/Cargocol";
@@ -18,12 +19,13 @@ function App() {
 
       <Router>
         <Routes>          
-          <Route path='/' element = {<Index />}/>
+          <Route path='/' element = {<Index_e />}/>
           <Route path='Index_e' element = {<Index_e />}/>
           <Route path='Camiones_e' element = {<Camiones_e />}/>
           <Route path='Carga' element = {<Carga />}/>
           <Route path='Index' element = {<Index />}/>          
           <Route path='Login' element = {<Login />}/>          
+          <Route path='Registro' element = {<Registro />}/>          
           <Route path='Empresas' element = {<Empresas />}/>
           <Route path='Cargocol' element = {<Cargocol />}/>
           <Route path='Cargocol1' element = {<Cargocol_1 />}/>
--- src/pages/auth/perfil_empresas/Index_e.js
+++ src/pages/auth/perfil_empresas/Index_e.js
@@ -77,8 +77,8 @@ const Index_e = () => {
                         <p class="font-normal text-gray-700 dark:text-gray-400">Capacidad de carga:</p>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Carga actual:</p>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Consumo de gasolina:</p>
-                        <a href="Cargocol" class=" mt-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-gray-300">
-                            Consultar Empresa
+                        <a href="" class=" mt-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-gray-300">
+                            Consultar Camion
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
@@ -99,7 +99,7 @@ const Index_e = () => {
                         <p class="font-normal text-gray-700 dark:text-gray-400">Carga actual:</p>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Consumo de gasolina:</p>
                         <a href="Ditransa" class=" mt-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-gray-300">
-                            Consultar Empresa
+                            Consultar Camion
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
@@ -120,7 +120,7 @@ const Index_e = () => {
                         <p class="font-normal text-gray-700 dark:text-gray-400">Carga actual:</p>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Consumo de gasolina:</p>
                         <a href="#" class=" mt-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-gray-300">
-                            Consultar camion
+                            Consultar Camion
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
@@ -141,7 +141,7 @@ const Index_e = () => {
                         <p class="font-normal text-gray-700 dark:text-gray-400">Carga actual:</p>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Consumo de gasolina:</p>
                         <a href="#" class=" mt-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-gray-300">
-                            Consultar camion
+                            Consultar Camion
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
